56446472f2c31a68af85f8473a0907b00e52eb6a
Ken Courville <krcourville@gmail.com>
2014-03-16 18:07:22 -0600

    Complete oo spec

--- spec/javascripts/oop_spec.js
+++ spec/javascripts/oop_spec.js
@@ -6,19 +6,79 @@ describe("Object Oriented JavaScript:", function(){
     **/
     //
     // Person
-    window.Person = function(args){
-      args = args || {};
-      this._firstName = args.firstName;
-      this._lastName = args.lastName;
-    };
+    window.Person = function(){};
     Person.prototype = {
       constructor: Person, 
       getFirstName : function(){ return _firstName; },
-      setFirstName : function(value){ _firstName = value; }
+      setFirstName : function(value){ _firstName = value; },
+      getJobDescription : function(){  }
     };
+    //
+    // Firefighter
+    window.Firefighter = function(){
+      this._axeswung = false;
+    };
+    Firefighter.prototype = Object.create( Person.prototype);    
+    Firefighter.prototype.swingAxe = function(){
+        this._axeswung = true;
+    };
+    Firefighter.prototype.isAxeSwung = function(){ 
+      return this._axeswung; 
+    };
+    Firefighter.prototype.getJobDescription  = function(){
+      return "Rescues kittens in distress.";
+    };
+    //
+    // PoliceOfficer
+    window.PoliceOfficer = function(){
+      this._ticketwritten = false;
+    };
+    PoliceOfficer.prototype = Object.create( Person.prototype );
+    PoliceOfficer.prototype.writeTicket = function(){
+        this._ticketwritten = true;
+    };
+    PoliceOfficer.prototype.isTicketWritten = function(){ 
+        return this._ticketwritten; 
+    };
+    PoliceOfficer.prototype.getJobDescription = function(){
+        return "Enforces the law";
+    };    
+
   });
   afterEach(function(){
     delete window.Person;
+    delete window.Firefighter;
+    delete window.PoliceOfficer;
+  });
+
+  describe("Instantiation and Inheritance:", function(){
+    var 
+      p = null,
+      fp = null,
+      po = null
+    ;
+    beforeEach(function(){
+      p = new Person();
+      fp = new Firefighter();
+      po = new PoliceOfficer();
+    });
+    it("Person should be an instance of Person", function(){
+      expect(p instanceof Person).toBe(true);
+    });
+    it("Firefighter should be an instance of Person", function(){
+      expect(fp instanceof Person).toBe(true);
+    });
+    it("Firefighter should be an instance of Firefighter", function(){
+      expect(fp instanceof Firefighter).toBe(true);
+    });
+    it("Firefighter can have its own extensions", function(){
+      fp.swingAxe();
+      expect(fp.isAxeSwung()).toBe(true);
+    });
+    it("PoliceOfficer can have it own extensions", function(){
+      po.writeTicket();
+      expect(po.isTicketWritten()).toBe(true);
+    });
   });
 
   describe("Encapsulation:", function(){
@@ -39,6 +99,29 @@ describe("Object Oriented JavaScript:", function(){
     it("a private variable can be accessed and mutated", function(){
       p.setFirstName(testData.firstName);
       expect(p.getFirstName()).toBe(testData.firstName);
+    });
+  });
+
+  describe("Polymorphism:", function(){
+    var 
+      p = null,
+      ff = null,
+      po = null
+    ;
+    beforeEach(function(){
+      p = new Person();
+      ff = new Firefighter();
+      po = new PoliceOfficer();
+    });
+
+    it("Parent class getJobDescription should not be defined", function(){
+      expect(p.getJobDescription()).toBeUndefined();
+    });
+    it("Child class FireFighter.getJobDescription should be defined", function(){
+      expect(ff.getJobDescription()).toBeDefined();
+    });
+    it("Parent class PoliceOfficer.getJobDescription should be defined", function(){
+      expect(po.getJobDescription()).toBeDefined();
     });
   });
 });
